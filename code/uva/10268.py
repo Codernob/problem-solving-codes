51366bb5c8385edf5f7829df197548cf976a553d
Codernob <arnob319@gmail.com>
2021-09-03 23:51:56 +0600

    updated uva 10268

--- code/uva/10268.py
+++ code/uva/10268.py
@@ -1,12 +1,20 @@
-#TLE; need to use horne's method
 while True:
     try:
         x = int(input())
         coe = list(map(int,input().split()))
-        n = len(coe)
-        ans = 0
-        for i in range(n-1):
-            ans += (coe[i]*(n-i-1)) * pow(x,n-i-2)
+
+        # the following method gives TLE
+        #n = len(coe)
+        #ans = 0
+        # for i in range(n-1):
+        #     ans += (coe[i]*(n-i-1)) * pow(x,n-i-2)
+
+        # for optimization, we use horner's rule
+        # https://en.wikipedia.org/wiki/Horner's_method
+        n = len(coe) -1
+        ans = n * coe[0]
+        for i in range(1,n):
+            ans = x*ans + (n-i)*coe[i]
         print(ans)
     except EOFError:
         break
